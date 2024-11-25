8688de5e077d6675e6c0eb4a42e88b006d56e625
christataa <christa_hernandez@dlsu.edu.ph>
2024-11-25 22:33:33 +0800

    changed loop structure

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 #include "kernel.c"
 
 extern void scalarmul(int n, float* arr1, float* arr2, float* ptr);
@@ -14,58 +13,60 @@ double get_time_in_seconds() {
 }
 
 int main() {
-    
     int n = 0;
-
-    printf("Enter size of vectors: ");
-    scanf("%d",&n);
-
-    float* vec1 = (float*)malloc(n * sizeof(float));
-    float* vec2 = (float*)malloc(n * sizeof(float));
-
-
+    int choice = 0;
+    int loops = 0;
+    int i = 0;
+    double start, end, endtotal;
     float var3 = 0;
     float* ptr = &var3; // Pointer to store the result of 1 dot product between vec1 and vec2
+    float* vec1 = NULL;
+    float* vec2 = NULL;
 
-    srand(time(NULL));
-
-    // Initialize vectors with random values between 0 and 9
-    for (int i = 0; i < n; i++) {
-        vec1[i] = rand() % 10;
-        vec2[i] = rand() % 10;
-    }
-
-    // Print the randomized vectors
-    printf("Vector A: ");
-    for (int i = 0; i < n; i++) {
-        printf("%.2f ", vec1[i]);
-    }
-    printf("\n");
-
-    printf("\n\nVector B: ");
-    for (int i = 0; i < n; i++) {
-        printf("%.2f ", vec2[i]);
-    }
-    printf("\n");
-
-
-    int choice = 0;
-    int i = 0;
-    double start;
-    double end;
-    int innerchoice = 0;
-    double endtotal = 0;
     do {
-        printf("\nC [1]  |  x86 assembly [2]  | Exit [3]\nEnter choice: ");
-        scanf("%d", &choice); 
+        printf("\nEnter size of vectors: ");
+        scanf("%d", &n);
 
-        switch (choice) {
-            case 1: // Call C kernel
-                endtotal = 0;
-                printf("Enter amount of loops: ");
-                scanf("%d",&innerchoice);
+        vec1 = (float*)malloc(n * sizeof(float));
+        vec2 = (float*)malloc(n * sizeof(float));
 
-                for(i = 0; i < innerchoice; i++){
+        srand(time(NULL));
+
+        for (int i = 0; i < n; i++) {
+            vec1[i] = ((float)rand() / RAND_MAX) * 10.0;
+            vec2[i] = ((float)rand() / RAND_MAX) * 10.0;
+        }
+
+        printf("\nVector A: ");
+        for (int i = 0; i < n; i++) {
+            printf("%.2f ", vec1[i]);
+        }
+        printf("\n");
+
+        printf("Vector B: ");
+        for (int i = 0; i < n; i++) {
+            printf("%.2f ", vec2[i]);
+        }
+
+        do {
+            // Step 2: Ask for kernel choice
+            printf("\n\n[1] C\n[2] x86 assembly\n[3] Exit\nEnter your choice: ");
+            scanf("%d", &choice);
+
+            if (choice == 3) {
+                printf("Exited.\n");
+                break;
+            }
+
+            // Step 3: Ask for number of loops
+            printf("\nEnter amount of loops: ");
+            scanf("%d", &loops);
+
+            endtotal = 0;
+
+            for (i = 0; i < loops; i++) {
+                if (choice == 1) {
+                    // C kernel
                     printf("\nCalling C kernel...\n");
                     start = get_time_in_seconds();
                     scalarmul_c(n, vec1, vec2, ptr);  // C kernel call
@@ -74,20 +75,9 @@ int main() {
                     endtotal += end;
                     printf("Result from C kernel: %.2f\n", *ptr);
                     printf("Time taken (C kernel): %.8f seconds\n", end);
-                }
-
-                    printf("\nAverage execution time for %d loops",innerchoice);
-                    printf("\nAverage Time taken (C kernel): %.8f seconds\n", endtotal/innerchoice);
-                   
-                break;
-
-            case 2: // Call x86 assembly kernel
-                endtotal = 0;
-                printf("Enter amount of loops: ");
-                scanf("%d",&innerchoice);
-
-                for(i = 0; i < innerchoice; i++){
-                     printf("\nCalling x86 assembly kernel...\n");
+                } else if (choice == 2) {
+                    // Assembly kernel
+                    printf("\nCalling x86 assembly kernel...\n");
                     start = get_time_in_seconds();
                     scalarmul(n, vec1, vec2, ptr);  // Assembly kernel call
                     end = get_time_in_seconds();
@@ -95,24 +85,35 @@ int main() {
                     endtotal += end;
                     printf("Result from assembly kernel: %.2f\n", *ptr);
                     printf("Time taken (Assembly kernel): %.8f seconds\n", end);
-                 }
-                    
-                    printf("\nAverage execution time for %d loops",innerchoice);
-                    printf("\nTime taken (Assembly kernel): %.8f seconds\n", endtotal/innerchoice);
-                break;
+                } else {
+                    printf("Invalid kernel choice. Please select again.\n");
+                    break;
+                }
+            }
 
-            case 3: // Exit
+            if (choice == 1 || choice == 2) {
+                printf("\nAverage execution time for %d loops: %.8f seconds\n", loops, endtotal / loops);
+            }
+
+            printf("\n\n[1] Try another kernel with the same vector size\n[2] Choose a new vector size\n[3] Exit\nEnter your choice: ");
+            scanf("%d", &choice);
+
+            if (choice == 2) {
+                free(vec1);
+                free(vec2);
+                break; 
+            } else if (choice == 3) {
                 printf("Exited.\n");
-                break;
+                free(vec1);
+                free(vec2);
+                return 0; 
+            }
 
-            default: 
-                printf("Enter a valid choice!\n");
-                break;
-        }
+        } while (choice == 1); // Loop back to kernel choice
 
-    } while (choice != 3);  // Repeat until exit
+    } while (choice == 2); // Loop back to vector size
 
-    free(vec1); 
+    free(vec1);
     free(vec2);
 
     return 0;
